51cde6432d8a5bb96d07b68e8633320b31bd29c0
Gabriel Cumare Montilla <gacm95218@gmail.com>
2024-05-24 06:17:13 +0000

    First commit of Counter (still having to work on it

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+let interval;
+
+const Counter = () => {
+  const [theVariable1, setTheVariable1] = useState(0);
+
+  const seconder = () => {
+    setTheVariable1((prev) => prev + 1);
+  };
+
+  if (!interval) {
+    interval = setInterval(seconder, 1000);
+  }
+
+  const formattedCount = theVariable1.toString().padStart(7, "0");
+
+  const displayformattedCount = formattedCount.split("")
+
+  return (
+    <div>
+      <div className="card text-center">
+        <div className="card-header text-white">
+          <h1>Seconds Counter</h1>
+        </div>
+        <div className="card-body">
+          
+          <h5 className="card-title text-white fs-1 d-flex justify-content-center">
+            <span><i class="fa-regular fa-clock"></i></span>
+            
+            {displayformattedCount.map((element) => {
+              return (<div><span className="boxNumber">{element}</span></div>)
+            })}
+          </h5>
+        </div>
+        <div className="card-footer text-body-secondary text-white">|</div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
